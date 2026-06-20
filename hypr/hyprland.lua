49cd12c52c8a178a8e7e34ad1672d6096445f9dd
Cauã R. Pereira <7aauac@gmail.com>
2026-06-20 00:06:15 -0300

    Refatorar keybindings: organizar e adicionar opções de redimensionamento de janela

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -260,16 +260,16 @@ hl.device({
 ---- KEYBINDINGS ----
 ---------------------
 
-local mainMod           = "SUPER"
-local closeWindowBind   = hl.bind(mainMod .. " + Q", hl.dsp.window.close())
+local mainMod = "SUPER"
 
-hl.bind(mainMod .. " + T",          hl.dsp.exec_cmd(terminal))
--- hl.bind(mainMod .. " + M",          hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
-hl.bind(mainMod .. " + E",          hl.dsp.exec_cmd(fileManager))
-hl.bind(mainMod .. " + J",          hl.dsp.window.float({ action = "toggle" }))
-hl.bind(mainMod .. " + Space",      hl.dsp.exec_cmd(menu))
-hl.bind(mainMod .. " + P",          hl.dsp.window.pseudo())
-hl.bind(mainMod .. " + F",  hl.dsp.window.fullscreen({ mode = "fullscreen" }))
+hl.bind(mainMod .. " + T",      hl.dsp.exec_cmd(terminal))
+hl.bind(mainMod .. " + Q",      hl.dsp.window.close())
+hl.bind(mainMod .. " + M",      hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
+hl.bind(mainMod .. " + E",      hl.dsp.exec_cmd(fileManager))
+hl.bind(mainMod .. " + J",      hl.dsp.window.float({ action = "toggle" }))
+hl.bind(mainMod .. " + Space",  hl.dsp.exec_cmd(menu))
+hl.bind(mainMod .. " + P",      hl.dsp.window.pseudo())
+hl.bind(mainMod .. " + F",      hl.dsp.window.fullscreen({ mode = "fullscreen" }))
 
 hl.bind(mainMod .. " + left",  hl.dsp.focus({ direction = "left" }))
 hl.bind(mainMod .. " + right", hl.dsp.focus({ direction = "right" }))
@@ -291,12 +291,12 @@ hl.bind(mainMod .. " + mouse_up",   hl.dsp.focus({ workspace = "e-1" }))
 hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
 hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
-hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
-hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true })
-hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),     { locked = true, repeating = true })
-hl.bind("XF86AudioMicMute",     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),   { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"),                  { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"),                  { locked = true, repeating = true })
+hl.bind("XF86AudioRaiseVolume",     hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
+hl.bind("XF86AudioLowerVolume",     hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true })
+hl.bind("XF86AudioMute",            hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),     { locked = true, repeating = true })
+hl.bind("XF86AudioMicMute",         hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),   { locked = true, repeating = true })
+hl.bind("XF86MonBrightnessUp",      hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"),                  { locked = true, repeating = true })
+hl.bind("XF86MonBrightnessDown",    hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"),                  { locked = true, repeating = true })
 
 hl.bind("XF86AudioNext",  hl.dsp.exec_cmd("playerctl next"),       { locked = true })
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
@@ -331,6 +331,10 @@ hl.bind(mainMod .. " + Y", hl.dsp.exec_cmd("kitty yazi"))
 
 -- Resize columns
 hl.bind(mainMod .. " + N", hl.dsp.layout("colresize +conf"))
+
+-- Segure SUPER + [+] ou [-] para redimensionar a janela como uma animação
+hl.bind(mainMod .. " + equal", hl.dsp.layout("colresize +0.1"), { repeating = true })
+hl.bind(mainMod .. " + minus", hl.dsp.layout("colresize -0.1"), { repeating = true })
 
 
 -----------------
